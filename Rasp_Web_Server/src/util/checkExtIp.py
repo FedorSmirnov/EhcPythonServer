33212b8ddb8fc55054a7e5ccd5cf96c6197b4a62
fedor <fedorsmirnov89@gmail.com>
2014-03-03 19:09:41 +0100

    the local ip is now also checked and updated

--- Rasp_Web_Server/src/util/checkExtIp.py
+++ Rasp_Web_Server/src/util/checkExtIp.py
@@ -6,26 +6,32 @@ Created on Jan 24, 2014
 
 import subprocess
 import threading
+import re
 import urllib
 import urllib2
 
 current_ext_ip =''
+current_local_ip = ''
 ip_check_interval = 1800
 webserver_ip_url = "http://hiwi.jochen-bauer.net/EhcServer/public/rasp/updateip"
 
-def update_ext_ip(cur_ip):
-    value = {"ip":cur_ip}
+def update_ext_ip(cur_ex_ip, cur_loc_ip):
+    value = {"ip":cur_ex_ip, "ip_local":cur_loc_ip}
     data = urllib.urlencode(value)
     print urllib2.urlopen(webserver_ip_url, data).read()
 
 def checkIP():
+    # check the external ip and update if necessary
     global current_ext_ip
-    resp_string = subprocess.check_output(['curl', '-s', 'icanhazip.com']).rstrip()
+    resp_string_ext = subprocess.check_output(['curl', '-s', 'icanhazip.com']).rstrip()
     
-    
-    if (current_ext_ip != resp_string):
-        current_ext_ip = resp_string
-        update_ext_ip(current_ext_ip)
+    global current_local_ip 
+    resp_string_local_check = subprocess.check_output('ifconfig')
+    resp_string_local = re.findall(r'wlan0\s*.+inet\s\w+:(\d+\.\d+\.\d+\.\d+)', resp_string_local_check, re.DOTALL)[0] 
+    if (current_ext_ip != resp_string_ext or current_local_ip != resp_string_local):
+        current_ext_ip = resp_string_ext
+        current_local_ip = resp_string_local
+        update_ext_ip(current_ext_ip, current_local_ip)
     t = threading.Timer(ip_check_interval, checkIP)
     t.setDaemon(True)
     t.start()
